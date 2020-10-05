f18ec3380bf66d940aa8a7511730b8c76df11f9d
Jim Knape <Jimwinthropknape@gmail.com>
2020-10-05 10:09:03 -0700

    optimizes naive solution for missingNumber for runtime; does not compute with Gauss' formula

--- missingNumber.js
+++ missingNumber.js
@@ -13,23 +13,13 @@ Follow up: Could you implement a solution using only O(1) extra space complexity
 
 
 var missingNumber = function(nums) {
-  // i: an array of numbers
-  // o: the missing number
-  // c: in constant time if possible
-  // e: if input array was empty
-
-  // [3,0,1]
-  
-
-  
-  // sort the nums array
   nums.sort((a, b) => a - b);
-  // iterate over the sorted array
+  if (nums[nums.length -1] !== nums.length) {
+    return nums.length;
+  };
   for (let i = 0; i < nums.length; i++) {
-        // if the value does not equal the index, return the index
       if (nums[i] !== i) {
         return i;
       }
   }
-  return nums.length;
 };
