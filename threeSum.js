70f3fb1147e9a925b560345d439840479bb7bd26
Jim Knape <Jimwinthropknape@gmail.com>
2020-10-09 09:01:19 -0700

    adds prompts for mergeIntervals and threeSums

---
+++ threeSum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+/* 15. 3Sum: https://leetcode.com/problems/3sum/
+
+Given an array nums of n integers, are there elements a, b, c in nums
+such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Notice that the solution set must not contain duplicate triplets.
+
+NOTE: This is an essential interview prompt, one which allows you to
+demonstrate your ability to reduce time complexity. Solve this in whatever
+way you are able to today, using "brute force" (ignoring efficiency) if necessary.
+We will revisit this problem later in JSP, after re/learning algorithms that will
+help you approach this problem with time complexity in mind.
+
+In Leetcode, the solution is locked for Premium subscribers. We will Slack a solution
+link 15 minutes before the end of the pair module so you and your partner can
+review the solution together.
+
+*/
+
+var threeSum = function(nums) {
+    
+};
